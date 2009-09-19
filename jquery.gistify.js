09f5ff5edf53c3bed7b7419c9362d04c2c90c558
Ben Scofield <git@turrean.com>
2009-09-19 18:14:40 -0400

    only add the github stylesheet once, regardless of the number of gists replaced

--- jquery.gistify.js
+++ jquery.gistify.js
@@ -9,7 +9,12 @@
       if (gist_id) {
         link.wrap('<div id="embedded-gist-'+gist_id[1]+'">');
         jQuery.getJSON(href+'.json?callback=?', function(data) {
-          $('head').append('<link rel="stylesheet" href="' + data.stylesheet + '" />');
+          var stylesheet_needed = true;
+          $('head link[rel=stylesheet]').each(function() {
+            if (this.href == data.stylesheet) { stylesheet_needed = false; }
+          })
+          
+          if (stylesheet_needed) {$('head').append('<link rel="stylesheet" href="' + data.stylesheet + '" />');}
           $('div#embedded-gist-'+gist_id[1]).replaceWith(data.div);
         });
       }
